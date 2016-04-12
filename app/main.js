0d9c4f5ce2207e7e3dd91d8caa9180818344ba34
JohnMagtoto <johnmagtoto@gmail.com>
2016-04-12 15:26:23 +0800

    Fixed Angular Scrpt cannot find names error; refs #2

--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
 "use strict";
 // bootstrap core AppComponent
+///<reference path="../node_modules/angular2/typings/browser.d.ts"/>
 var browser_1 = require('angular2/platform/browser');
 var app_component_1 = require('./app.component');
 browser_1.bootstrap(app_component_1.AppComponent);
